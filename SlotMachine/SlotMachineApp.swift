20a55f3cd9cf18814295f3072c32818e1b6bf7c9
Minseok Kim <min.cfa13@gmail.com>
2023-07-28 21:49:17 +0900

    This is our first commit!

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Min on 2023/07/27.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
